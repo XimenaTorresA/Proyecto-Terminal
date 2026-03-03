443bcddd504b0a1f6c55b449c44f3bf4760cc9dd
XimenaTorresA <etorresa1801@alumno.ipn.mx>
2026-03-02 23:20:02 -0600

    chore(db): integrar TypeORM y configurar conexión a PostgreSQL en AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    //Configuración para conectar con PostgreSQL
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'password123',
+      database: 'sistema_recomendador_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Sincroniza automáticamente los modelos con la BD
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
